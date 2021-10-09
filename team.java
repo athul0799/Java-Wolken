5a6ec8bc5ceed3aea34ebdd7055af3e5d16fbd7d
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-09 13:34:51 +0530

    Create team.java

---
+++ team.java
@@ -0,0 +1,14 @@
+class Teams {
+	
+	String name;
+	int position;
+	String jerseyColour;
+	
+	static void strength(){
+		System.out.println("Team is strong in batting");
+	}
+	
+	static void weakness(){
+		System.out.println("Team is weak in fielding");
+	}
+}
