ebbbeb417c54b4b12209f6ab13400ba2fd72c4c4
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-09 13:31:36 +0530

    Create task4.java

---
+++ task4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class task4 {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter the number to find out day");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        if(num == 1){
+            System.out.println("Sunday");
+            }
+        else if(num == 2){
+            System.out.println("Monday");
+            }
+        else if(num == 3){
+            System.out.println("Tuesday");
+            }
+            else if(num == 4){
+            System.out.println("Wednesday");
+            }
+            else if(num == 5){
+            System.out.println("Thursday");
+            }
+            else if(num == 6){
+            System.out.println("Friday");
+            }
+            else if(num == 7){
+            System.out.println("Saturday");
+            }
+            else {
+                System.out.println("Invalid. Enter num between 1-7");            }
+                
+    }
+}
