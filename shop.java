9f8e41f899defc5e9b4d3715842c25c0baeff5d7
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-09 13:35:51 +0530

    Create shop.java

---
+++ shop.java
@@ -0,0 +1,14 @@
+class shop {
+	
+	String name;
+	int shopno;
+	String floor;
+	
+	static void open(){
+		System.out.println("Shop is open");
+	}
+	
+	static void close(){
+		System.out.println("Shop is closed");
+	}
+}
