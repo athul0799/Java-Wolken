e5aa0af74f24306782410a9b555ba1bcdf1ba51e
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-09 13:36:29 +0530

    Create task8.java

---
+++ task8.java
@@ -0,0 +1,16 @@
+class task8 {
+	
+	     task8(){
+     	System.out.println("This is default constructor");
+		}
+		
+		public void hello(){
+			System.out.println("hello");
+		}
+		public static void main(String[] args){
+			task8 obj = new task8();
+			obj.hello();
+		
+		
+	}
+}
