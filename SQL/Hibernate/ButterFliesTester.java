f549b2f88c83c44da7ecb2d766dbffc0876aed1e
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-20 23:20:28 +0530

    Create ButterFliesTester.java

---
+++ SQL/Hibernate/ButterFliesTester.java
@@ -0,0 +1,21 @@
+package com.wolken.springexample;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.wolken.butterflies.entity.ButterflyDetails;
+public class ButterfliesTester 
+{
+public static void main(String[] args )
+    {
+ Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        SessionFactory factory = config.buildSessionFactory();
+        Session session = factory.openSession();
+    	ButterDetails details = (ButterDetails)session.get(ButterflyDetails.class, 1);
+    	System.out.println(details);
+    	session.close();
+    	factory.close();
+    }
+}
