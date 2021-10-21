e57bb435a14d2f5849e2c2392cd3690603143cbc
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-21 23:55:20 +0530

    Create MarketDetailsDAO.java

---
+++ SQL/Hibernate/marketDetails/MarketDetailsDAO.java
@@ -0,0 +1,10 @@
+package com.wolken.market.dao;
+
+public interface MarketDetailsDAO {
+	package com.wolken.market.dao;
+	import com.wolken.market.entity.MarketDetails;
+
+		public boolean save(MarketDetails details);
+		public boolean update(MarketDetails details);
+		public void getById(int id);
+		boolean String delete(int id);  }
