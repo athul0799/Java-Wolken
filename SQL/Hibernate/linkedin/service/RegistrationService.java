2e34d681af6ae9b9ac6edb685bf6c3fd8c3b5dce
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-26 01:27:17 +0530

    Update RegistrationService.java

--- SQL/Hibernate/linkedin/service/RegistrationService.java
+++ SQL/Hibernate/linkedin/service/RegistrationService.java
@@ -1,6 +1,9 @@
 package com.wolken.linkedin.service;
+import com.wolken.linkedin.dto.LoginDTO;
 import com.wolken.linkedin.dto.UserDTO;
 
 public interface RegistrationService {
 String validateAndSave(UserDTO dto);
+  String validateAndLogin(LoginDTO ldto);
+  String validateAndUpdatePassword(String email)
 }
