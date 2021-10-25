7ad2a5206afc9f53712e901ce4c7ca79e2239813
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-26 01:51:30 +0530

    Update RegistrationDAO.java

--- SQL/Hibernate/linkedin/dao/RegistrationDAO.java
+++ SQL/Hibernate/linkedin/dao/RegistrationDAO.java
@@ -4,4 +4,6 @@ import com.wolken.linkedin.entity.UserEntity;
 public interface RegistrationDAO {
 
 public int save(UserEntity entity);
+public LinkedinEntity getByEmail(String email);
+public int updatePassword(UserEntity entity);
 }
