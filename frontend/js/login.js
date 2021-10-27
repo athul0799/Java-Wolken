52bd06e353e1ae5eb69c100842cc56bbad40a17a
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-28 00:32:45 +0530

    Create login.js

---
+++ frontend/js/login.js
@@ -0,0 +1,23 @@
+function validateLogin()
+{
+    var email=$("#email").val();
+    var pass=$("#pass").val();
+    var emailValid = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+    var letters = /^[a-z]+$/;
+    var letterNum = /^[a-zA-Z0-9]+$/;
+     
+
+   if(email.match(emailValid) && email.length>=6 && email.length<18 && email.length != "") {
+        $("#errEmail").html('');
+          
+   if(password.match(letterNum) && password.length>8 && password.length<16 && password.length !="")
+        {
+            $("#errPass").html('');
+        } else {
+   
+            $("#errPass").html('Password not valid must have all types of characters');
+        }
+    } else {
+        $("#errEmail").html('Email invalid');
+    }
+}
