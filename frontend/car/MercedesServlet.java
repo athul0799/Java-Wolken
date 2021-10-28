91e83b864cc5d840bd3d22562180633e9b503e15
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-29 00:13:59 +0530

    Create MercedesServlet.java

---
+++ frontend/car/MercedesServlet.java
@@ -0,0 +1,26 @@
+package com.wolken.car;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MercedesServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>"
+				+ "<head>"
+				+ "<title>Tesla</title>"
+				+ "</head>"
+				+ "<body>" Mercedes-Benz, commonly referred to as Mercedes, is a German luxury automotive brand. Mercedes-Benz produces consumer luxury vehicles and commercial vehicles.
+				+ "</body>"
+				+ "</html>");
+		writer.close();
+		
+	}	
+}
