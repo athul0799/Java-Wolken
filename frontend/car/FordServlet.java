3ff2ffc0da4529018676732fff9d9fb5745134ec
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-29 00:30:15 +0530

    Create FordServlet.java

---
+++ frontend/car/FordServlet.java
@@ -0,0 +1,26 @@
+package com.wolken.car;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FordServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>"
+				+ "<head>"
+				+ "<title>Ford</title>"
+				+ "</head>"
+				+ "<body>"Ford Motor Company is an American multinational automobile manufacturer headquartered in Dearborn, Michigan, United States. It was founded by Henry Ford and incorporated on June 16, 1903. The company sells automobiles and commercial vehicles under the Ford brand, and luxury cars under its Lincoln luxury brand.
+				+ "</body>"
+				+ "</html>");
+		writer.close();
+		
+	}	
+}
