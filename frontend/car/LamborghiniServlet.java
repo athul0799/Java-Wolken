d6b1f1b85bbfe9265a3e43c493de339d3c2131b2
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-10-29 00:18:39 +0530

    Create LamborghiniServlet.java

---
+++ frontend/car/LamborghiniServlet.java
@@ -0,0 +1,26 @@
+package com.wolken.car;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LamborghiniServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>"
+				+ "<head>"
+				+ "<title>Tesla</title>"
+				+ "</head>"
+				+ "<body>"  The company is owned by the Volkswagen Group through its subsidiary Audi. It is an italian brand
+				+ "</body>"
+				+ "</html>");
+		writer.close();
+		
+	}	
+}
