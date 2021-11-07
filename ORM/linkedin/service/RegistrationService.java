3b7d4032dd725502f46465658b8c3724ad31b457
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-11-08 00:00:48 +0530

    Create RegistrationService.java

---
+++ ORM/linkedin/service/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.wolken.linkedin.service;
+
+public interface RegistrationService {
+	
+	String validateAndSave(UserDTO dto);
+	  String validateAndLogin(LoginDTO ldto);
+	  String validateAndUpdatePassword(String email);
+
+}
