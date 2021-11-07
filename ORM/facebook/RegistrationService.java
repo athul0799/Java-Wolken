1f3210732cd05d359261d9ffb2f340516f44575c
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-11-08 01:03:01 +0530

    Delete RegistrationService.java

--- ORM/facebook/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.wolken.facebook.service;
-
-public interface RegistrationService {
-	
-	String validateAndSave(UserDTO dto);
-	  String validateAndLogin(LoginDTO ldto);
-	  String validateAndUpdatePassword(String email);
-
-}
