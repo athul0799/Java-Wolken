e2a6f1833b83dd9bc868805bf44a4db2d58c915f
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-11-08 01:13:52 +0530

    Create UserDTO.java

---
+++ ORM/hackerrank/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.wolken.hackerrank.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+public class UserDTO {
+	@Getter
+	@Setter
+	@ToString
+
+		private int Id;
+		private String Name;
+		private String email;
+		private String Password;
+		private String Designation;
+		private String dob;
+		
+
+}
