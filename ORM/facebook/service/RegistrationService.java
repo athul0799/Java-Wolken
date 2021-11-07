f8fcdbeaae936d91dccd7a131c7f0ca36475fc22
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-11-08 01:03:21 +0530

    Create RegistrationService.java

---
+++ ORM/facebook/service/RegistrationService.java
@@ -0,0 +1,13 @@
+package com.wolken.facebook.service;
+
+public interface RegistrationService {
+	
+	String validateAndSave(UserDTO dto);
+	  String validateAndLogin(LoginDTO ldto);
+	  String validateAndUpdatePassword(String email);
+
+}
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Secu
